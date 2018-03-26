02baa1a5f296c6627e6716295bef9fd2f1bc3c89
peawyoyoyin <peawyoyoyin@gmail.com>
2018-03-26 21:31:34 +0700

    add course entity

---
+++ src/entity/course.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm'
+
+Entity()
+export class Course {
+  @PrimaryColumn({type: 'varchar', length: 7})
+  courseID: string
+
+  @Column({type: 'varchar', length: 30})
+  name: string
+
+  @Column({type: 'int'})
+  credit: number
+}
