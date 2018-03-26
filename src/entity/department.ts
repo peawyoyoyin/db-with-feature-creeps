41f4bab3f0743fb6f6d30430f81b79ef1b094cf3
peawyoyoyin <peawyoyoyin@gmail.com>
2018-03-26 21:53:18 +0700

    add entity file for department

---
+++ src/entity/department.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
+import { Faculty } from './faculty'
+
+Entity()
+export class Department {
+  @PrimaryGeneratedColumn({type: 'int'})
+  departmentID: number
+
+  @ManyToOne(type => Faculty)
+  faculty: Faculty
+
+  @Column({type: 'varchar', length: 30})
+  name: string
+
+}
