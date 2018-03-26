58eeca066fddc0a14bd083c69b7542bc3a6edb90
peawyoyoyin <peawyoyoyin@gmail.com>
2018-03-26 21:33:58 +0700

    add evaluation type entity

---
+++ src/entity/evaluation-type.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryColumn, Column, PrimaryGeneratedColumn } from 'typeorm'
+
+Entity()
+export class EvaluationType {
+  @PrimaryGeneratedColumn({type: 'int'})
+  typeID: number
+
+  @Column({type: 'int'})
+  weight: number
+
+  @Column({type: 'varchar', length: 50})
+  description: string
+}
