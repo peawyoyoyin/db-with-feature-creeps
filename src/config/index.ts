956d81e98f77167ecb201c660d3fc8f7c9066819
peawyoyoyin <peawyoyoyin@gmail.com>
2018-03-26 15:14:12 +0700

    config now use deeper fallbacks

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,21 @@
-import { ProjectConfig } from "./config.d";
+import { ProjectConfig, ExpressConfig } from "./config.d";
+import { ConnectionOptions } from "typeorm";
 
 const config = ((): ProjectConfig => {
   try {
-    return { ...require('./config.default'), ...require('./config')}
+    const config = require('./config')
+    const defaults = require('./config.default')
+
+    return { 
+      express: <ExpressConfig> {
+        ...defaults.express,
+        ...config.express,
+      },
+      orm: <ConnectionOptions> {
+        ...defaults.orm,
+        ...config.orm,
+      }
+     }
   } catch(error) {
     console.log('unable to find "src/config/config.ts", falling back to "src/config/config.default.ts"')
     return require('./config.default')
