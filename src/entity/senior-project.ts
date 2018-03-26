d0978fde759222070270e1a3deece9514799ca41
peawyoyoyin <peawyoyoyin@gmail.com>
2018-03-26 21:29:36 +0700

    add missing fields for senior project entity

--- src/entity/senior-project.ts
+++ src/entity/senior-project.ts
@@ -3,5 +3,11 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 Entity()
 export class SeniorProject {
   @PrimaryGeneratedColumn({type: 'int'})
-  projectID: number  
+  projectID: number
+
+  @Column({type: 'varchar', length: '100'})
+  topic: string
+
+  @Column({type: 'int'})
+  year: number
 }
