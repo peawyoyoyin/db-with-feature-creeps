7077ccaba7ca50384a87121037988c7e1bf3c7f3
peawyoyoyin <peawyoyoyin@gmail.com>
2018-04-09 12:30:47 +0700

    add pug as templating engine because it's pug

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,28 @@ import { Connection, createConnection } from 'typeorm'
 
 import { Student } from './entity/student'
 
-// const app = express()
-
 ;(async () => {
   const connection: Connection = await createConnection(config.orm)
   const repo = connection.getRepository(Student)
   // const test = repo.create({
-  //   studentID: '5834515112',
-  //   firstName: 'John',
-  //   lastName: 'Doe',
-  //   citizenID: '1579900777777',
-  //   year: 2558,
-  //   nationality: 'TH'
-  // })
-  // repo.save(test)
-})()
+    //   studentID: '5834515112',
+    //   firstName: 'John',
+    //   lastName: 'Doe',
+    //   citizenID: '1579900777777',
+    //   year: 2558,
+    //   nationality: 'TH'
+    // })
+    // repo.save(test)
+  })()
+  
+const app = express()
+app.set('view engine', 'pug')
+app.use(bodyParser.json())
 
-// app.use(bodyParser.json())
-// app.get('/', (req, res) => {
-//   res.end('Hello World!')
-// })
-
-// app.listen(config.express.port, () => {
-//   console.log(`listening on port ${config.express.port}`)
-// })
+app.get('/', (req, res) => {
+  res.render('index', { title: 'DB WITH FEATURE CREEPS', message: 'HELLO FEATURE CREEPS' })
+})
+  
+app.listen(config.express.port, () => {
+  console.log(`listening on port ${config.express.port}`)
+})
