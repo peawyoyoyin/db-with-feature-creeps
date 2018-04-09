7f422380779937d377a84b8fd52f926716bef3cd
peawyoyoyin <peawyoyoyin@gmail.com>
2018-04-09 12:31:32 +0700

    remove comment on how workaround were used as it isn't used anymore

--- src/entity/course-instance.ts
+++ src/entity/course-instance.ts
@@ -3,13 +3,6 @@ import { Course } from './course';
 import { Semester } from './semester'
 import { Section } from './section'
 
-/**
- * The primary, auto-generated field instanceID exists
- * because foreign keys as primary keys isn't supported yet in typeORM
- *
- * Here we use Index to indicate that the FK fields will be unique
- */
-
 @Entity()
 export class CourseInstance {
   @ManyToOne(type => Course, course => course.instances, {onDelete: 'CASCADE', primary: true})
