7aa167cc4eab39708f9a46d5d403e280a7075fbf
peawyoyoyin <peawyoyoyin@gmail.com>
2018-04-09 15:06:16 +0700

    add sidebar layout similar to reg chula with a good logo xD

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,25 @@ import { Connection, createConnection } from 'typeorm'
 
 import { Student } from './entity/student'
 
-;(async () => {
-  const connection: Connection = await createConnection(config.orm)
-  const repo = connection.getRepository(Student)
-  // const test = repo.create({
-    //   studentID: '5834515112',
-    //   firstName: 'John',
-    //   lastName: 'Doe',
-    //   citizenID: '1579900777777',
-    //   year: 2558,
-    //   nationality: 'TH'
-    // })
-    // repo.save(test)
-  })()
+// ;(async () => {
+//   const connection: Connection = await createConnection(config.orm)
+//   const repo = connection.getRepository(Student)
+//   // const test = repo.create({
+//     //   studentID: '5834515112',
+//     //   firstName: 'John',
+//     //   lastName: 'Doe',
+//     //   citizenID: '1579900777777',
+//     //   year: 2558,
+//     //   nationality: 'TH'
+//     // })
+//     // repo.save(test)
+//   })()
   
 const app = express()
 app.set('view engine', 'pug')
 app.use(bodyParser.json())
+
+app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'DB WITH FEATURE CREEPS', message: 'HELLO FEATURE CREEPS' })
