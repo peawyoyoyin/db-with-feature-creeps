4942853525ad78c1b4bf9f1d6bd23c29102ec1fe
Sivakorn Chanpitayanukulkij <ch.sivakorn@gmail.com>
2018-04-17 17:30:27 +0700

    Teacher.ts, db.ts

--- src/entity/teacher.ts
+++ src/entity/teacher.ts
@@ -1,26 +1,59 @@
-import { Entity, PrimaryColumn, Column, OneToMany, ManyToMany, ManyToOne } from 'typeorm'
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  OneToMany,
+  ManyToMany,
+  ManyToOne
+} from 'typeorm'
+import { Length, Min, IsNumberString } from 'class-validator'
+import { validate } from '~/utils'
 import { Student } from './student'
 import { Section } from './section'
 import { Course } from './course'
 import { Department } from './department'
-import { SeniorProject } from '~/entity/senior-project';
-import { Evaluation } from '~/entity/evaluation';
+import { SeniorProject } from '~/entity/senior-project'
+import { Evaluation } from '~/entity/evaluation'
 
+interface StudentArgs {
+  teacherID: string
+  firstName: string
+  lastName: string
+  abbrName: string
+  citizenID: string
+}
 @Entity()
 export class Teacher {
+  constructor(args: StudentArgs) {
+    if (args === undefined) return
+    this.teacherID = args.teacherID
+    this.firstName = args.firstName
+    this.lastName = args.lastName
+    this.abbrName = args.abbrName
+    this.citizenID = args.citizenID
+    validate(this)
+  }
+
   @PrimaryColumn({ type: 'varchar', length: 10 })
+  @Length(10, 10, { message: 'teacherID length must be 10 characters' })
+  @IsNumberString()
   teacherID: string
 
   @Column({ type: 'varchar', length: 30 })
+  @Length(1, 30)
   firstName: string
 
   @Column({ type: 'varchar', length: 30 })
+  @Length(1, 30)
   lastName: string
 
   @Column({ type: 'varchar', length: 3 })
+  @Length(3, 3)
   abbrName: string
 
   @Column({ type: 'varchar', length: 13 })
+  @Length(13, 13, { message: 'citizenID length must be 13 characters' })
+  @IsNumberString()
   citizenID: string
 
   @OneToMany(type => Student, student => student.supervisor)
@@ -35,7 +68,9 @@ export class Teacher {
   @OneToMany(type => Course, course => course.owner)
   coursesOwned: Course[]
 
-  @ManyToOne(type => Department, department => department.teachers, {onDelete: 'SET NULL'})
+  @ManyToOne(type => Department, department => department.teachers, {
+    onDelete: 'SET NULL'
+  })
   department: Department
 
   @ManyToMany(type => Evaluation, evaluation => evaluation.evaluators)
