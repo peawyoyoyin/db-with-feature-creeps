f49c27621dc80939846ef3a9d0bb8a39e5b1b243
peawyoyoyin <peawyoyoyin@gmail.com>
2018-04-18 10:38:03 +0700

    errors object generated on validate is now logged at server-side

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ import { validateSync, } from 'class-validator'
 export function validate(obj) {
   const errors = validateSync(obj)
   if (errors.length > 0) {
+    console.log(errors)
     const allMsg = errors.map(error => {
       const msg = Object.values(error.constraints).join(', ')
       return msg
