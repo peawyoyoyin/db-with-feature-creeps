524c4a9c25fe798a3d99fd4b0f97755d58cab5a9
nai <wattanai.tha@gmail.com>
2018-04-18 18:22:08 +0700

    refactor detail route into its own file

--- src/routes/course/index.ts
+++ src/routes/course/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import manage from './manage'
 import enroll from './enroll'
 import search from './search'
+import detail from './detail'
 import db from '~/db'
 import { AdvancedConsoleLogger } from 'typeorm'
 
@@ -10,39 +11,7 @@ const router = express.Router()
 router.use('/enroll', enroll)
 router.use('/manage', manage)
 router.use('/search', search)
-
-router.get('/detail/:id', (req, res) => {
-  let course = {
-    courseID: '2110117',
-    courseName: 'SOME SUBJ',
-    credits: 3,
-    sections: [
-      {
-        sectionNumber: 1,
-        capacity: 40,
-        teacher: {
-          firstName: 'John',
-          lastName: 'Snoe',
-          abbrName: 'JSN'
-        }
-      },
-      {
-        sectionNumber: 2,
-        capacity: 20,
-        teacher: {
-          firstName: 'Jorn',
-          lastName: 'Though',
-          abbrName: 'JTH'
-        }
-      }
-    ]
-  }
-
-  res.render('course/detail', {
-    title: req.params.id,
-    course
-  })
-})
+router.use('/detail', detail)
 
 router.get('/pay', (req, res) => {
   res.render('course/pay', { title: 'Pay Fee' })
