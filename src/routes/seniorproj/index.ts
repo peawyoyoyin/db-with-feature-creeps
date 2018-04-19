8993159ea1f720a11d709ee98b2267bc1c05e9f9
phootip <phootip.t@gmail.com>
2018-04-19 18:03:16 +0700

    add seniorproj browse & update form

--- src/routes/seniorproj/index.ts
+++ src/routes/seniorproj/index.ts
@@ -3,7 +3,19 @@ import * as express from 'express'
 const router = express.Router()
 
 router.get('/browse', (req, res) => {
-  res.render('seniorproj/browse', { title: 'Browse Senior Projects' })
+  const { projectID, topic, year, superVisorTeacherID } = req.query
+  let searchProjectID = projectID
+  let searchTopic = topic
+  let searchYear = year
+  let searchSuperVisorTeacherID = superVisorTeacherID
+  res.render('seniorproj/browse', { 
+    title: 'Browse Senior Projects',
+    searchProjectID: searchProjectID,
+    searchTopic: searchTopic,
+    searchYear: searchYear,
+    years: [2558,2559,2560],
+    searchSuperVisorTeacherID: searchSuperVisorTeacherID
+  })
 })
 
 router.get('/register', (req, res) => {
@@ -11,7 +23,10 @@ router.get('/register', (req, res) => {
 })
 
 router.get('/update', (req, res) => {
-  res.render('seniorproj/update', { title: 'Update Senior Project Status' })
+  res.render('seniorproj/update', { 
+    title: 'Update Senior Project Status',
+    grades:["A","B+","B","C+","C","D+","D","F"]
+  })
 })
 
 
