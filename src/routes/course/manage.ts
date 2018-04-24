b83033128107c9b926425d4dc4a0a027cc4f26ec
nai <wattanai.tha@gmail.com>
2018-04-24 11:59:42 +0700

    implement studentNumber validation

--- src/routes/course/manage.ts
+++ src/routes/course/manage.ts
@@ -13,31 +13,46 @@ router.get('/', async (req, res) => {
     `,
     [studentID]
   )
-  if (studentRows.length === 1) {
-    const student = studentRows[0]
-    console.log(student)
-    const { studentID, firstName, lastName } = student
-    studentData = {
-      info: {
-        studentID,
-        firstName,
-        lastName,
-      },
-      subjects: [
-        {
-          courseID: '2110217',
-          courseName: 'SOME SUBJ',
-          section: 1
+  if (studentID === undefined || studentID === '') studentData = null
+  else {
+    if (studentRows.length === 1) {
+      const student = studentRows[0]
+      console.log(student)
+      const { studentID, firstName, lastName } = student
+      studentData = {
+        info: {
+          studentID,
+          firstName,
+          lastName
         },
-        {
-          courseID: '2110224',
-          courseName: 'SOME SUBJ LAB',
-          section: 33
+        subjects: [
+          {
+            courseID: '2110217',
+            courseName: 'SOME SUBJ',
+            section: 1
+          },
+          {
+            courseID: '2110224',
+            courseName: 'SOME SUBJ LAB',
+            section: 33
+          }
+        ]
+      }
+    } else {
+      studentData = {
+        info: {
+          studentID,
+          notFound: true
         }
-      ]
+      }
     }
   }
-  res.render('course/manage', { title: 'Manage Courses', studentData, studentID })
+
+  res.render('course/manage', {
+    title: 'Manage Courses',
+    studentData,
+    studentID
+  })
 })
 
 router.post('/', (req, res) => {
