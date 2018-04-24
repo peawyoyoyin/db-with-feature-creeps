3b34e108853eea67331775acbab1c96518f1646d
nai <wattanai.tha@gmail.com>
2018-04-24 11:41:29 +0700

    reimplement logic in enroll.ts

--- src/routes/course/enroll.ts
+++ src/routes/course/enroll.ts
@@ -7,73 +7,44 @@ const router = express.Router()
 
 interface EnrollData {
   studentID: string
-  courseID: string | Array<string>
-  courseSectNum: string | Array<string>
-  courseSectOption: string | Array<string>
-  courseSectNum2: string | Array<string>
+  courseID: any
+  courseSectNum: any
   semesterId: number
 }
 
 async function handleEnrolls(body: EnrollData) {
-  const {
-    studentID,
-    courseID,
-    courseSectNum,
-    courseSectOption,
-    courseSectNum2,
-    semesterId
-  } = body
+  let { studentID, courseID, courseSectNum, semesterId } = body
   if (typeof courseID === 'string') {
-    const course = await getCourseInstancePromise(courseID, semesterId)
-    if (course instanceof Error) {
-      return [course.message]
-    }
-    const { id: courseInstanceID } = course
-    const section = await getSectionPromise(
-      courseInstanceID,
-      <string>courseSectNum,
-      courseID
-    )
-    if (section instanceof Error) {
-      return [section.message]
-    }
-    const { id: sectionID, courseInstanceId } = section
-    const result = await insertStudyPromise(
-      studentID,
-      sectionID,
-      courseInstanceId
-    )
-    if (result instanceof Error) {
-      return [result.message]
-    }
-  } else {
-    const coursePromises = courseID.map(id =>
-      getCourseInstancePromise(id, semesterId)
-    )
-
-    const courses = await Promise.all(coursePromises)
-
-    const e = courses.filter(c => c instanceof Error).map(e => e.message)
-    if (e.length > 0) return e
-
-    const sectionPromises = courses.map((course, index) => {
-      const { id } = course
-      const sectionNumber = courseSectNum[index]
-      return getSectionPromise(id, sectionNumber, courseID[index])
-    })
-
-    const sections = await Promise.all(sectionPromises)
-    const e2 = sections.filter(s => s instanceof Error).map(e => e.message)
-    if (e2.length > 0) return e2
-
-    const insertPromises = sections.map(section => {
-      const { id: sectionID, courseInstanceId } = section
-      return insertStudyPromise(studentID, sectionID, courseInstanceId)
-    })
-    const results = await Promise.all(insertPromises)
-    const e3 = results.filter(r => r instanceof Error).map(e => e.message)
-    if (e3.length > 0) return e3
+    courseID = [courseID]
+    courseSectNum = [courseSectNum]
   }
+  const coursePromises = courseID.map(id =>
+    getCourseInstancePromise(id, semesterId)
+  )
+
+  const courses = await Promise.all(coursePromises)
+
+  const e = courses.filter(c => c instanceof Error).map((e: Error) => e.message)
+  if (e.length > 0) return e
+
+  const sectionPromises = courses.map((course: any, index) => {
+    const { id } = course
+    const sectionNumber = courseSectNum[index]
+    return getSectionPromise(id, sectionNumber, courseID[index])
+  })
+
+  const sections = await Promise.all(sectionPromises)
+  const e2 = sections.filter(s => s instanceof Error).map(e => e.message)
+  if (e2.length > 0) return e2
+
+  const insertPromises = sections.map(section => {
+    const { id: sectionID, courseInstanceId } = section
+    return insertStudyPromise(studentID, sectionID, courseInstanceId)
+  })
+  const results = await Promise.all(insertPromises)
+  const e3 = results.filter(r => r instanceof Error).map(e => e.message)
+  if (e3.length > 0) return e3
+
   return []
 }
 
