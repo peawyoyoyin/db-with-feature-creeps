eb923541a2c6b176193accde73538f8e23580804
nai <wattanai.tha@gmail.com>
2018-04-26 17:22:50 +0700

    fix validation in seniorproj/update page

--- src/routes/seniorproj/update.ts
+++ src/routes/seniorproj/update.ts
@@ -1,33 +1,66 @@
 import * as express from 'express'
+import { check, validationResult } from 'express-validator/check'
 import db from '~/db'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
   const rawTypes = await getAllEvaluationType()
-  const types = rawTypes.map(type => ({
-    text: type.description,
-    value: type.typeID
-  }))
+  const types = formatTypes(rawTypes)
   res.render('seniorproj/update', {
     title: 'Update Senior Project Status',
-    grades: ['A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F'],
-    types
+    types,
+    errors: []
   })
 })
 
-router.post('/', async (req, res) => {
-  console.log(req.body)
-  const { teacherID, projectID, type, grade, comment } = req.body
-  try {
-    // const rawTeacher = await getTeacher(teacherID)
-    await createEvaluation(projectID, teacherID, type, comment, grade)
-    // console.log(rawTeacher)
-    res.redirect('/seniorproj/update')
-  } catch (e) {
-    console.log(e)
+router.post(
+  '/',
+  [
+    check('grade')
+      .isNumeric()
+      .withMessage('Grade must be a number')
+  ],
+  async (req, res) => {
+    console.log(req.body)
+    // console.log(validationResult(req).array())
+    const { teacherID, projectID, type, grade, comment } = req.body
+    const errors = validationResult(req)
+      .array()
+      .map((e: any) => e.msg)
+
+    try {
+      await getProject(projectID)
+    } catch (e) {
+      errors.push(e.message)
+    }
+    try {
+      await getTeacher(teacherID)
+    } catch (e) {
+      errors.push(e.message)
+    }
+    try {
+      if (errors.length === 0) res.redirect('/')
+      else throw new Error()
+      await createEvaluation(projectID, teacherID, type, comment, grade)
+    } catch (e) {
+      const rawTypes = await getAllEvaluationType()
+      const types = formatTypes(rawTypes)
+      res.render('seniorproj/update', {
+        title: 'Update Senior Project Status',
+        types,
+        errors
+      })
+    }
   }
-})
+)
+
+function formatTypes(types) {
+  return types.map(type => ({
+    text: type.description,
+    value: type.typeID
+  }))
+}
 
 async function getAllEvaluationType() {
   const rawTypes = await db.evaluationType.query(`
@@ -47,8 +80,20 @@ async function getTeacher(teacherID) {
   if (rawTeacher.length === 0) throw new Error('Cannot find teacher')
   return rawTeacher[0]
 }
+
+async function getProject(projectID) {
+  const rawProject = await db.seniorProject.query(
+    `
+    SELECT * FROM senior_project
+    WHERE projectID = ?
+    `,
+    [projectID]
+  )
+  if (rawProject.length === 0) throw new Error('Cannot find project')
+  return rawProject[0]
+}
+
 async function createEvaluation(projectID, teacherID, typeID, comment, grade) {
-  // await db.teacher.queryRunner.startTransaction()
   const result = await db.evaluation.query(
     `
     insert into evaluation(comment,grade,projectProjectID,evaluationTypeTypeID)
@@ -64,7 +109,7 @@ async function createEvaluation(projectID, teacherID, typeID, comment, grade) {
     `,
     [insertId, teacherID]
   )
-  console.log(result)
+  // console.log(result)
 }
 
 export default router
